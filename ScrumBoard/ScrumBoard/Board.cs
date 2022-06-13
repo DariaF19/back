65ebc9ac037006d15d3ea10b6eb8a21aceb1f8e4
DariaF19 <80063167+DariaF19@users.noreply.github.com>
2022-06-13 15:02:44 +0300

    add board

---
+++ ScrumBoard/ScrumBoard/Board.cs
@@ -0,0 +1,69 @@
+using TaskBoard.source.task;
+using Task = TaskBoard.source.task.Task;
+
+namespace TaskBoard.source.board;
+
+public class Board
+{
+    private readonly List<Task> _taskPool;
+    private int _lastId = 0;
+
+    public Board()
+    {
+        _taskPool = new List<Task>();
+    }
+    
+    public int AddTask(string title, string? description = null)
+    {
+        int id = _lastId;
+        _taskPool.Add(new Task(_lastId++, title, description));
+        return id;
+    }
+
+    public Task? GetTaskById(int id)
+    {
+        return _taskPool.Find(t => t.Id == id);
+    }
+
+    public List<Task> GetTasks()
+    {
+        return _taskPool;
+    }
+    
+    public List<Task> GetTasks(Status status)
+    {
+        return _taskPool.FindAll(t => t.Status == status);
+    }
+
+    public void UpdateStatus(int id, Status status)
+    {
+        if (!_taskPool.Exists(t => t.Id == id))
+        {
+            throw new Exception($"Task with id {id} does not exist.");
+        }
+
+        int index = _taskPool.FindIndex(t => t.Id == id);
+        _taskPool[index].Status = status;
+    }
+
+    public void UpdateDescription(int id, string? description = null)
+    {
+        if (!_taskPool.Exists(t => t.Id == id))
+        {
+            throw new Exception($"Task with id {id} does not exist.");
+        }
+
+        int index = _taskPool.FindIndex(t => t.Id == id);
+        _taskPool[index].Description = description;
+    }
+    
+    public void DeleteTask(int id)
+    {
+        if (!_taskPool.Exists(t => t.Id == id))
+        {
+            throw new Exception($"Task with id {id} does not exist.");
+        }
+        int index = _taskPool.FindIndex(t => t.Id == id);
+        _taskPool.Remove(_taskPool[index]);
+    }
+}
